253d17298bcce01576100440a0bb038b62f7e4dd
Bekir <bekir911@hotmail.com>
2023-12-09 20:49:33 +0300

    remove editor.getValue().replace(/[ ]/g, "\n") because you can't put spaces

--- js/filters.js
+++ js/filters.js
@@ -195,7 +195,6 @@ async function saveEditorText() {
 
 function getEditorText(editor) {
     return editor.getValue()
-        .replace(/[ ]/g, "\n")
         .replace(/[^\n\d]/g, "")
         .replace(/\n{2,}/g, "\n")
         .trim();
