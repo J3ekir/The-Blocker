d3b5d089dc732edc203c7a81808984e2bfac90e7
Bekir <bekir911@hotmail.com>
2024-01-01 14:22:38 +0300

    add default editor setup

--- js/notes.js
+++ js/notes.js
@@ -1,5 +1,29 @@
 /* Heavily inspired by Raymond Hill's uBlock Origin */
 
 (async () => {
+    var settings = await chrome.storage.local.get([
+        "notes",
+    ]);
 
+    var cache = "";
+
+    const buttons = {
+        save: qs("#applyButton"),
+        import: qs("#importButton"),
+        export: qs("#exportButton"),
+        filePicker: qs("#filePicker"),
+    };
+
+    const codeMirrorOptions = {
+        autofocus: true,
+        foldGutter: true,
+        gutters: ["CodeMirror-foldgutter", "CodeMirror-linenumbers"],
+        lineNumbers: true,
+        lineWiseCopyCut: true,
+        scrollbarStyle: "overlay",
+        styleActiveLine: { nonEmpty: true },
+        configureMouse: _ => { return { addNew: false }; },
+    };
+
+    const noteEditor = new CodeMirror(qs("#noteEditor"), codeMirrorOptions);
 })();
