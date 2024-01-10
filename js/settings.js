26219b521d90b74de62a69078af7ef83b00cc64b
J3ekir <bekir911@hotmail.com>
2024-01-10 03:35:48 +0300

    add host_permission request

--- js/settings.js
+++ js/settings.js
@@ -5,12 +5,30 @@ parent.postMessage({
     title: document.title,
 }, "*");
 
+chrome.permissions.contains({
+    origins: ["https://www.technopat.net/sosyal/*"],
+}).then(granted => {
+    if (!granted) {
+        document.querySelector("#requestPermission").style.display = "flex";
+    }
+});
+
+qs("#requestPermission>button").addEventListener("click", requestPermission);
+
 chrome.storage.local.get().then(settings => {
     qsa("[data-setting-name]").forEach(elem => {
         elem.checked = settings[dom.attr(elem, "data-setting-name")];
         elem.addEventListener("change", settingChanged);
     });
 });
+
+function requestPermission() {
+    chrome.permissions.request({
+        origins: ["https://www.technopat.net/sosyal/*"],
+    }).then(() => {
+        qs("#requestPermission").style.display = "none";
+    });
+}
 
 function settingChanged(event) {
     const settingName = dom.attr(event.target, "data-setting-name");
