b7ea8319a45900ae6befa119f08c77fbcc3914b9
J3ekir <bekir911@hotmail.com>
2024-11-14 01:10:25 +0300

    improve storage getter in settings.js

--- js/settings.js
+++ js/settings.js
@@ -15,10 +15,12 @@ qs("#theme").addEventListener("change", event => {
     chrome.storage.local.set({ theme: event.currentTarget.value });
 });
 
-chrome.storage.local.get().then(settings => {
+const settingElements = qsa("[data-setting-name]");
+const settingKeys = Array.from(settingElements, elem => elem.getAttribute("data-setting-name"));
+chrome.storage.local.get(["theme", ...settingKeys]).then(settings => {
     qs("#theme").value = settings["theme"];
 
-    qsa("[data-setting-name]").forEach(elem => {
+    settingElements.forEach(elem => {
         elem.checked = settings[elem.getAttribute("data-setting-name")];
         elem.addEventListener("change", settingChanged);
     });
