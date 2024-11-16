e8f4316c38f85769def2dcf6a9654eed6e6532e8
J3ekir <bekir911@hotmail.com>
2024-11-17 00:29:13 +0300

    use optional chaining for qs and qsa functions

--- js/dom.js
+++ js/dom.js
@@ -1,11 +1,11 @@
 function qs(a, b) {
     return typeof a === "string"
         ? document.querySelector(a)
-        : a.querySelector(b);
+        : a.querySelector?.(b);
 };
 
 function qsa(a, b) {
     return typeof a === "string"
         ? document.querySelectorAll(a)
-        : a.querySelectorAll(b);
+        : a.querySelectorAll?.(b);
 };
