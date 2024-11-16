587b05ff07f8aad5ff011fa7b478093c58dd5323
J3ekir <bekir911@hotmail.com>
2024-11-17 02:52:42 +0300

    remove unnecessary setCSS call rely on storage onChanged listener for triggering

--- js/background.js
+++ js/background.js
@@ -3,7 +3,6 @@ import "./backgroundUtils.js";
 
 chrome.runtime.onInstalled.addListener(({ reason, temporary }) => {
     setDefaultSettings();
-    setCSS();
 
     if (reason === "install") {
         checkPermissions();
