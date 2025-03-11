6bd8f59360602a17228bbdfbcfeb8bb18ed56cba
J3ekir <bekir911@hotmail.com>
2025-03-11 11:47:27 +0300

    use spread operator to convert array-like objects

--- js/widget_tabs.js
+++ js/widget_tabs.js
@@ -42,7 +42,7 @@
 
 	function activateTab(event) {
 		const tab = event.currentTarget;
-		const index = Array.from(tab.parentElement.children).indexOf(tab);
+		const index = [...tab.parentElement.children].indexOf(tab);
 		qsa(":is([role='tab'],[role='tabpanel']).is-active").forEach(elem => elem.classList.remove("is-active"));
 		qsa(`:is([role='tab'],[role='tabpanel']):nth-child(${ index + 1 })`).forEach(elem => elem.classList.add("is-active"));
 
