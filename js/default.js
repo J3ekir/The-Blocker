0b216bca9a885f31d9d5c882c2c40ef4b96d37b2
J3ekir <bekir911@hotmail.com>
2026-05-29 17:02:46 +0300

    add CSS rules to hide specific elements in membe stats

--- js/default.js
+++ js/default.js
@@ -1,3 +1,39 @@
+/* https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/manifest.json/content_scripts#browser_compatibility */
+const css = `
+:is(.memberTooltip-stats, .memberHeader-stats) dl {
+	/* techolay */
+	/* yazılımlar */
+	&:has(> dd > [href^="/sosyal/indir"]),
+	/* rozetler */
+	&:has(> dd > [href$="/badges"]),
+	/* giriş serisi */
+	&:has(> dd > [href$="/daily-login-streaks"]),
+    /* ürünler */
+	&:has(> dd > [href=^"/sosyal/satis"]) {
+		display: none !important;
+	}
+
+    /* technopat */
+    /* medya */
+    &:has(> dd > [href^="/sosyal/medya"]),
+    /* blog */
+    &:has(> dd > [href^="/sosyal/blog"]),
+    /* blog girdisi yorumları*/
+    &:has(> dd > [href^="/sosyal/blog"]) + *,
+    /* rozetler */
+    &:has(> dd > [href$="/badges"]),
+    /* giriş serisi */
+	&:has(> dd > [href$="/daily-login-streaks"]) {
+		display: none !important;
+	}
+}
+`;
+
+chrome.runtime.sendMessage({
+    type: "insertCssString",
+    css,
+});
+
 (async () => {
 	waitForElement("#footer").then(executeDefaults);
 
