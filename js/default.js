c687ca934edc63a4e457b25f3a29bee859ddae66
J3ekir <bekir911@hotmail.com>
2026-06-07 11:34:43 +0300

    refactor CSS and JS for improved readability and consistency

--- js/default.js
+++ js/default.js
@@ -1,45 +1,45 @@
 (async () => {
 	waitForElement("#footer").then(() => {
-        executeDefaults();
+		executeDefaults();
 		ignoreUserStyles();
-    });
+	});
 
-    const noticeAds = [
-        "reklam",
-        "amazon",
-        "hepsiburada",
-        "n11",
-        "trendyol",
-        "kampanya",
-        "indirim",
-        "fırsat",
-        "haber",
-    ];
-    const frameSelector = ".ap-ss-avatarFrameContainer .avatar";
-    const seenFrameIds = new Set();
+	const noticeAds = [
+		"reklam",
+		"amazon",
+		"hepsiburada",
+		"n11",
+		"trendyol",
+		"kampanya",
+		"indirim",
+		"fırsat",
+		"haber",
+	];
+	const frameSelector = ".ap-ss-avatarFrameContainer .avatar";
+	const seenFrameIds = new Set();
 
-    function executeDefaults() {
-        qsa(".p-body-inner>.notices>.notice").forEach(elem => {
-            if (noticeAds.some(ad => elem.textContent.toLowerCase().includes(ad))) {
-                chrome.runtime.sendMessage({
-                    type: "dismissNotice",
-                    noticeId: elem.dataset.noticeId,
-                });
-            }
-        });
-    }
+	function executeDefaults() {
+		qsa(".p-body-inner>.notices>.notice").forEach(elem => {
+			if (noticeAds.some(ad => elem.textContent.toLowerCase().includes(ad))) {
+				chrome.runtime.sendMessage({
+					type: "dismissNotice",
+					noticeId: elem.dataset.noticeId,
+				});
+			}
+		});
+	}
 
-    function ignoreUserStyles() {
-        if (forum !== "technopat") { return; }
+	function ignoreUserStyles() {
+		if (forum !== "technopat") { return; }
 
-        qsa(frameSelector).forEach(elem => {
-            if (seenFrameIds.has(elem.dataset.userId)) { return; }
+		qsa(frameSelector).forEach(elem => {
+			if (seenFrameIds.has(elem.dataset.userId)) { return; }
 
-            seenFrameIds.add(elem.dataset.userId);
-            chrome.runtime.sendMessage({
-                type: "ignoreUserStyle",
-                url: `${ elem.href }/ignore-style`,
-            });
-        });
-    }
+			seenFrameIds.add(elem.dataset.userId);
+			chrome.runtime.sendMessage({
+				type: "ignoreUserStyle",
+				url: `${ elem.href }/ignore-style`,
+			});
+		});
+	}
 })();
